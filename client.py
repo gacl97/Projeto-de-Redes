ca23913776228f13cfd2fc60d2242d88f22d6fa9
Gustavo Augusto <43053386+gacl97@users.noreply.github.com>
2020-01-09 23:59:29 -0300

    Add files via upload

--- client.py
+++ client.py
@@ -2,8 +2,9 @@ import socket
 import os
 import sqlite3
 import time
+from tqdm import tqdm
 
-address = ('localhost', 7401)
+address = ('localhost', 7416)
 
 # Create sockets
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,7 +42,7 @@ def create_user():
         cursor.execute(find_user,[(username)])
         
         if(cursor.fetchall()):
-            again = input("This username already exists, do you want to try again?(y,n)")
+            again = input("This username already exists, do you want to try again? [y,n]")
             if(again.lower() == 'n'):
                 print("Returning to main menu!!")
                 time.sleep(1)
@@ -49,6 +50,7 @@ def create_user():
         else:
             break
     password = input("Enter password:")
+    print()
     password1 = input("Enter again your password:")
     while(password != password1):
         
@@ -60,14 +62,13 @@ def create_user():
     db.commit()
     print("Successful registration!!")
 
-
-def menu():
+def main_menu():
     while True:
 
         print("[1]- Login: ")
         print("[2]- Register")
         print("[3]- Exit")
-        op = input()
+        op = exception_option()
         if(op == '1'):
             username = login()
             if(username != "Fail"):
@@ -83,28 +84,108 @@ def menu():
             time.sleep(1)
             break
 
+def show_client_files(username):
 
+    with sqlite3.connect("USERS.db") as db:
+        cursor = db.cursor()
+    find_file = ("SELECT files.filename, files.filesize FROM files INNER JOIN user ON files.user_id = user.userID AND user.username = ?;")
+    cursor.execute(find_file,[(username)])
+    results = cursor.fetchall()
+    print("-------------- "+ username +" FILES -------------")
+
+    if(len(results) != 0):
+        for result in results:
+            print("Name: ",result[0], " Size: ", result[1])
+    else:
+        print("You don't have any files yet!!")
+    
+    print("------------------------------------------")
+    print()
+
+def exception_option():
+
+    while True:
+        try:
+            option = int(input())
+            
+            return str(option)
+        except:
+            pass
+        print("Option not available!")
+
+def exception_files():
+
+    while True:
+        try:
+            file_name = input("Enter file or directory name: ")
+            file = open(file_name,"rb")
+            return [file,file_name]
+        except:
+            pass
+        print("File or directory not available!")
+        again = input("Try again? [y,n]")
+        if(again.lower() == 'n'):
+            print("Returning to menu!!")
+            time.sleep(1)
+            return [False,False]
+
+def progress_bar(file_size):
+
+    for i in tqdm(range(file_size)):
+        i += 1024
+    
+
+def menu():
+
+    print("[1]- Show available files: ")
+    print("[2]- Upload")
+    print("[3]- Close the program")
 
 def transfer_files(username):
     # Echo
     print("Bem-vindo: ", username[0][1])
     while True:
         try:
+            menu()
+            op = exception_option()
+            if(op == '1'):
+                show_client_files(username[0][1])
+            elif(op == '2'):
 
-            text = input("Informe o arquivo: ")
-            file = open(text,"rb")
-            aux_size = os.stat(text)
-            file_size = str(aux_size.st_size)
-            client_socket.send(file_size.encode())
-            time.sleep(0.5)
-            client_socket.send(text.encode())
-            client_socket.send(file.read(aux_size.st_size))
-            file.close()
-            if (text == "sair"):
+                file, file_name = exception_files()
+                if(not file):
+                    continue
+                aux_size = os.stat(file_name)
+                
+                aux_file_name = file_name.split("/")
+                file_name = aux_file_name[-1]
+
+                file_size = str(aux_size.st_size)
+                client_socket.send(file_size.encode())
+                time.sleep(0.2)
+                client_socket.send(str(username[0][0]).encode())
+                time.sleep(0.2)
+                client_socket.send(username[0][1].encode())
+                time.sleep(0.2)
+                client_socket.send(file_name.encode())
+                time.sleep(0.2)
+
+                result = client_socket.recv(1024)
+                
+                if(result.decode() == "False"):
+                    client_socket.send(file.read(aux_size.st_size))
+                    progress_bar(aux_size.st_size)
+                    print("Successfully Uploaded!!")
+                    print()
+                    time.sleep(0.1)
+                else:
+                    print("You have already uploaded this file!!")
+                file.close()
+            elif (op == '3'):
                 client_socket.close()
                 break
         except:
             print("O servidor foi desativado.")
             client_socket.close()
             break
-menu()
+main_menu()
--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
-
+import time
 import socket
 import _thread
 import os
+import sqlite3
+from tqdm import tqdm
 
-address = ('localhost', 7401)
+address = ('localhost', 7416)
 
 # Create sockets
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,37 +15,82 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(address)
 server_socket.listen(1)
 
-def get_name(name):
+def get_name(name, username):
     new_name = name.decode().split(".")
-    new_name[0] += "_"
     new_name[1] = "." + new_name[1]
     return (new_name[0] + new_name[1])
+
+
+def check_file_existence(username, file_name):
+    with sqlite3.connect("USERS.db") as db:
+        cursor = db.cursor()
+    find_file = ("SELECT files.filename, files.filesize FROM files INNER JOIN user ON files.user_id = user.userID AND files.filename = ? AND user.username = ? ;")
+    cursor.execute(find_file,[(file_name),(username)])
+    results = cursor.fetchone()
+    if(results):
+        print("You have already uploaded this file!!")
+        return True
+    else:
+        return False
+
+
+def save_path_file_db(username, userID, file_path, file_name, file_size):
+
+    with sqlite3.connect("USERS.db") as db:
+        cursor = db.cursor()
+    insertData = '''INSERT INTO files(fullpath,filename,filesize,user_id) VALUES(?,?,?,?)'''
+    cursor.execute(insertData,[(file_path),(file_name),(file_size),(userID)])
+    db.commit()
+    
+
 
 def client_thread(server_input, address):
 
     while True:
+        
         file_size = server_input.recv(1024)
         file_size = file_size.decode()
         file_size = int(file_size)
-
+        
+        userID = server_input.recv(1024)
+        userID = int(userID.decode())
+        username = server_input.recv(1024)
+        username = username.decode()
         file_name = server_input.recv(1024)
-        new_file_name = get_name(file_name) 
+        new_file_name = get_name(file_name,username) 
 
-        new_file = open(new_file_name,'wb')
-        aux_size = 0
-        while aux_size < file_size:
-            data = server_input.recv(1024)
-            new_file.write(data)
-            aux_size += len(data)
-        new_file.close()
-            
-        if (data == "sair"):
-            print (address, "se desconectou.")
-            server_input.close()
-            return
-        print("Upload feito com sucesso por: ", end = "")
-        print(address)
+        client_folder = os.getcwd() + "/Server Files/" + username
+        if(not os.path.isdir(client_folder)):
+            os.mkdir(client_folder)
+        
+        save_path = client_folder + "/" + new_file_name
+        
+        result = check_file_existence(username,new_file_name)
 
+        server_input.send(str(result).encode())
+        if(not result):
+
+            save_path_file_db(username,userID,save_path,new_file_name,file_size)
+            new_file = open(save_path,'wb')
+            aux_size = 0
+            progress_bar = tqdm(total=file_size)
+            while aux_size < file_size:
+                data = server_input.recv(1024)
+                new_file.write(data)
+                progress_bar.update(aux_size)
+                aux_size += len(data)
+            new_file.close()
+            print("Successfully Uploaded by ", username)
+            print()
+
+def create_files_folder():
+    folder = os.getcwd()
+    folder += "/Server Files"
+    if(not os.path.isdir(folder)):
+        os.mkdir(folder)
+
+
+create_files_folder()
 # Create New Threads
 while True:
     server_input, address = server_socket.accept()
